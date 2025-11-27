7c95510349509ca60b6c541f20f1e0e1eb70eda4
mahesh.k3 <mahesh.k@beamx.co>
2025-11-27 22:04:37 +0530

    count unique values in a sorted array

--- Problem_solving_patterns/multipointer/04_count_unique_values.js
+++ Problem_solving_patterns/multipointer/04_count_unique_values.js
@@ -1,4 +1,19 @@
 /*
-Implement a funtion called countUniqueVvlues which accepts a sorted array, and counts
+Implement a funtion called countUniqueValues which accepts a sorted array, and counts
 the unique values in the array. there can be negative numbers in the array, but it will always be soreted
 */
+
+function countUniqueValues(arr) {
+  if (arr.length === 0) return 0;
+
+  let i = 0;
+  for (j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+}
+
+console.log(countUniqueValues([1, 1, 1, 3, 3, 4, 5, 6, 7, 8]));
