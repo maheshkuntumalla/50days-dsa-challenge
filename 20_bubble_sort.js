ad912478906c8bc3318662d1e2ee09084c0b6063
Mahesh K <maheshkuntumalla2001@gmail.com>
2025-12-23 23:50:56 +0530

    optimized version for nearly sorted arrays

--- 20_bubble_sort.js
+++ 20_bubble_sort.js
@@ -10,3 +10,23 @@ function bubbleSort(arr) {
 }
 
 console.log(bubbleSort([6, 3, 2, 7, 9, 1]));
+
+// optimized version for nearly sorted arrays
+
+function optimizedBubbleSort(arr) {
+  let noSwap;
+  for (let i = 0; i < arr.length - 1; i++) {
+    noSwap = true;
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        noSwap = false;
+      }
+    }
+    console.log("hi");
+    if (noSwap) break;
+  }
+  return arr;
+}
+
+console.log(optimizedBubbleSort([9, 3, 4, 5, 6]));
