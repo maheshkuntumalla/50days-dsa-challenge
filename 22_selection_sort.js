e3b5a323f49806cc96908407898623b58d243b8c
Mahesh K <maheshkuntumalla2001@gmail.com>
2025-12-29 00:22:40 +0530

    selection sorting

---
+++ 22_selection_sort.js
@@ -0,0 +1,16 @@
+//SELECTION SORT
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    [arr[i], arr[min]] = [arr[min], arr[i]];
+  }
+  return arr;
+}
+
+console.log(selectionSort([3, 5, 2, 7, 1]));
